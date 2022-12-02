77261241e4b8e75b6da8f8e976af696c86de9c6e
Adrian Zgorzalek <adrian@adek.io>
2022-12-02 10:55:33 +0000

    Refactor reading function

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,4 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use crate::utils::read_lines;
 
 pub fn run() {
     let lines = read_lines("in/day1.in").unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod day1;
+mod utils;
 
 fn main() {
     day1::run();
