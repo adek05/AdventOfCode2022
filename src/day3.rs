7218245fac7e876da842331fd041b2b9bd25160b
Adrian Zgorzalek <adrian@adek.io>
2022-12-03 12:01:31 +0000

    Day 3, part 2

--- src/day3.rs
+++ src/day3.rs
@@ -7,8 +7,8 @@ use std::collections::HashSet;
 /// ```
 pub fn priority(c: char) -> u32 {
     match c {
-        c if c >='a' && c <='z' => 1 + c as u32 - 'a' as u32,
-        c if c >='A' && c <='Z' => 27 + c as u32 - 'A' as u32,
+        c if c >= 'a' && c <= 'z' => 1 + c as u32 - 'a' as u32,
+        c if c >= 'A' && c <= 'Z' => 27 + c as u32 - 'A' as u32,
         _ => panic!("Non-digit ASCII char given: {}", c),
     }
 }
@@ -18,8 +18,8 @@ pub fn priority(c: char) -> u32 {
 /// assert_eq!(find_bad_item(&[1,2,3,4,4,5,6,4]), 4);
 /// ```
 pub fn find_bad_item(rucksack: &[u32]) -> u32 {
-    let part1: HashSet<&u32> = HashSet::from_iter(&rucksack[0 .. rucksack.len() / 2]);
-    let part2 = HashSet::from_iter(&rucksack[rucksack.len() / 2 ..]);
+    let part1: HashSet<&u32> = HashSet::from_iter(&rucksack[0..rucksack.len() / 2]);
+    let part2 = HashSet::from_iter(&rucksack[rucksack.len() / 2..]);
     **part1.intersection(&part2).next().unwrap()
 }
 
@@ -27,11 +27,31 @@ pub fn run() {
     let lines = read_lines("in/day3.in").unwrap();
 
     let mut rucksacks: Vec<Vec<u32>> = vec![];
-    
+
     for line in lines {
         let l = line.unwrap();
         rucksacks.push(l.chars().map(priority).collect());
     }
-    println!("Day 3.1: {}", rucksacks.iter().map(|x| find_bad_item(&x)).sum::<u32>());
+    println!(
+        "Day 3.1: {}",
+        rucksacks.iter().map(|x| find_bad_item(&x)).sum::<u32>()
+    );
 
+    let groups = rucksacks.chunks(3);
+
+    let mut priorities_of_groups = vec![];
+    for group in groups {
+        priorities_of_groups.push(
+            *group
+                .to_owned()
+                .into_iter()
+                .map(|rucksack| -> HashSet<u32> { HashSet::from_iter(rucksack) })
+                .reduce(|acc, elem| acc.intersection(&elem).into_iter().cloned().collect())
+                .unwrap()
+                .iter()
+                .next()
+                .unwrap(),
+        );
+    }
+    println!("Day 3.2: {}", priorities_of_groups.into_iter().sum::<u32>(),);
 }
