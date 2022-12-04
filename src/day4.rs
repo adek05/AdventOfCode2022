c85be587361f29b1427d5fc218a064f63e5d58d9
Adrian Zgorzalek <adrian@adek.io>
2022-12-04 11:26:04 +0000

    Day 4, part 2

--- src/day4.rs
+++ src/day4.rs
@@ -21,19 +21,32 @@ fn contains_impl(r1: &Elf, r2: &Elf) -> bool {
     false
 }
 
+pub fn overlaps(r1: &Elf, r2: &Elf) -> bool {
+    r1.start <= r2.start && r2.start <= r1.end ||
+    r2.start <= r1.start && r1.start <= r2.end ||
+    contains(r1, r2)
+}
+
 pub fn run() {
     let lines = read_lines("in/day4.in").unwrap();
 
     let mut fully_contains_pairs = 0;
+    let mut overlap_pairs = 0;
 
     for line in lines {
         let l = line.unwrap();
         scan!(&l; (let s1: i32, "-", let e1:i32, ",", let s2: i32, "-", let e2: i32) => {
-            if contains(&Elf{start: s1, end: e1}, &Elf{start: s2, end: e2}) {
+            let pair1 = Elf{start: s1, end: e1};
+            let pair2 = Elf{start: s2, end: e2};
+            if contains(&pair1, &pair2) {
                 fully_contains_pairs += 1;
+            }
+            if overlaps(&pair1, &pair2) {
+                overlap_pairs += 1;
             }
         }).unwrap();
     }
 
     println!("Day 4, part 1: {}", fully_contains_pairs);
+    println!("Day 4, part 2: {}", overlap_pairs);
 }
