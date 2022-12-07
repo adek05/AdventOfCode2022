81cb97f17495df431beea1a80a1419b652d03840
Adrian Zgorzalek <adrian@adek.io>
2022-12-07 22:10:41 +0000

    Day 8 prep

---
+++ src/day8.rs
@@ -0,0 +1,5 @@
+use crate::utils::read_lines;
+
+pub fn run() {
+    let lines = read_lines("in/day8.in").unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ fn main() {
     // aoc::day4::run();
     // aoc::day5::run();
     // aoc::day6::run();
-    aoc::day7::run();
+    // aoc::day7::run();
+    aoc::day8::run();
 }
