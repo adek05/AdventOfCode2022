903c196148211b29bed0dc243f450554f30c29b9
Adrian Zgorzalek <adrian@adek.io>
2022-12-10 07:49:21 +0000

    Day 10

---
+++ src/day10.rs
@@ -0,0 +1,46 @@
+use crate::utils::read_lines;
+
+pub fn run() {
+    let lines = read_lines("in/day10.in").unwrap();
+    let mut register = 1;
+    let mut clock = 1;
+
+    let mut register_values = vec![];
+    register_values.push(1);
+
+    for line in lines {
+        let l = line.unwrap();
+        scan!(&l;
+          ("noop") => {
+            register_values.push(register);
+            clock+=1;
+          },
+          ("addx", let delta: i32) => {
+            register_values.push(register);
+            register_values.push(register);
+            register += delta;
+            clock+=2;
+          },
+        )
+        .unwrap();
+    }
+
+    let part1 = [20, 60, 100, 140, 180, 220]
+        .iter()
+        .map(|idx| *idx as i64 * register_values[*idx] as i64)
+        .sum::<i64>();
+    println!("Day 10, part 1 {}", part1);
+
+    for y in 0..6 {
+        for x in 0..40 {
+            let cycle = y * 40 + x;
+            let sprite_pos = register_values[cycle + 1];
+            if (cycle as i32 % 40 - sprite_pos).abs() <= 1 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!("");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,6 @@ fn main() {
     // aoc::day6::run();
     // aoc::day7::run();
     // aoc::day8::run();
-    aoc::day9::run();
+    // aoc::day9::run();
+    aoc::day10::run();
 }
