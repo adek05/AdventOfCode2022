7f915741e0d8e72dc3b5c4e353fdc19baaded8b1
Adrian Zgorzalek <adrian@adek.io>
2022-12-30 21:59:17 +0000

    Day 22, part 2

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,7 @@
 use crate::utils::read_lines;
 
+const SIZE: usize = 50;
+
 #[derive(Debug)]
 enum Instruction {
     Clockwise,
@@ -7,14 +9,13 @@ enum Instruction {
     Forward(u32),
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Orientation {
     Right,
     Down,
     Left,
     Up,
 }
-// (1, 0), (0, 1), (-1, 0), ()
 
 #[derive(Debug)]
 struct Position {
@@ -42,7 +43,7 @@ impl Position {
                 Orientation::Left => ((new_pos.0 + dimx - 1) % dimx, new_pos.1),
                 Orientation::Up => (new_pos.0, (dimy + new_pos.1 - 1) % dimy),
             };
-            if &map[new_pos.1][new_pos.0] != &MapLocation::Padding {
+            if map[new_pos.1][new_pos.0] != MapLocation::Padding {
                 break;
             }
         }
@@ -52,6 +53,160 @@ impl Position {
             y: new_pos.1,
             orientation_idx: self.orientation_idx,
         }
+    }
+
+    pub fn forward2(&self, map: &[Vec<MapLocation>]) -> Position {
+        let old_x = self.x;
+        let old_y = self.y;
+        let orientation = ORIENTATIONS[self.orientation_idx];
+        let (x, y) = match ORIENTATIONS[self.orientation_idx] {
+            Orientation::Right => (old_x + 1, old_y),
+            Orientation::Down => (old_x, old_y + 1),
+            Orientation::Left => (old_x - 1, old_y),
+            Orientation::Up => (old_x, old_y - 1),
+        };
+        if map[y][x] != MapLocation::Padding {
+            return Position {
+                x,
+                y,
+                orientation_idx: self.orientation_idx,
+            };
+        }
+        let res = if y == 0 {
+            assert_eq!(orientation, Orientation::Up);
+            if x <= 100 {
+                // 1U -> 6R
+                Position {
+                    x: 1,
+                    y: 151 + (x - 1) % 50,
+                    orientation_idx: 0, // Right
+                }
+            } else {
+                // 2U -> 6U
+                Position {
+                    x: 1 + (x - 1) % 50,
+                    y: 200,
+                    orientation_idx: 3, // Up
+                }
+            }
+        } else if x == 151 {
+            assert_eq!(orientation, Orientation::Right);
+            // 2R -> 5L
+            Position {
+                x: 100,
+                y: 150 - (y - 1) % 50,
+                orientation_idx: 2, // Left
+            }
+        } else if x > 100 && y > 50 {
+            // 2D -> 3L
+            if orientation == Orientation::Down {
+                Position {
+                    x: 100,
+                    y: 51 + (x - 1) % 50,
+                    orientation_idx: 2, // Left
+                }
+            } else if y <= 100 {
+                // 3R -> 2U
+                assert_eq!(orientation, Orientation::Right);
+                Position {
+                    x: 101 + (y - 1) % 50,
+                    y: 50,
+                    orientation_idx: 3, // Up
+                }
+            } else {
+                assert!((101..=150).contains(&y));
+                assert_eq!(orientation, Orientation::Right);
+                // 5R -> 2L
+                Position {
+                    x: 150,
+                    y: 50 - (y - 1) % 50,
+                    orientation_idx: 2, // Left
+                }
+            }
+        } else if y > 150 && x > 50 {
+            if orientation == Orientation::Down {
+                // 5D -> 6L
+                Position {
+                    x: 50,
+                    y: 151 + (x - 1) % 50,
+                    orientation_idx: 2, // Left
+                }
+            } else {
+                // 6R -> 5U
+                assert!(x > 50);
+                assert_eq!(orientation, Orientation::Right);
+                Position {
+                    x: 51 + (y - 1) % 50,
+                    y: 150,
+                    orientation_idx: 3, // Up
+                }
+            }
+        } else if y > 200 {
+            // 6D -> 2D
+            Position {
+                x: 101 + (x - 1) % 50,
+                y: 1,
+                orientation_idx: 1, // Down
+            }
+        } else if x == 0 {
+            if y >= 151 {
+                // 6L -> 1D
+                Position {
+                    x: 51 + (y - 1) % 50,
+                    y: 1,
+                    orientation_idx: 1, // Down
+                }
+            } else {
+                // 4L -> 1R
+                assert!(y >= 101);
+                Position {
+                    x: 51,
+                    y: 50 - (y - 1) % 50,
+                    orientation_idx: 0, // Right
+                }
+            }
+        } else {
+            assert!(y <= 100 && x <= 50);
+            if orientation == Orientation::Up {
+                // 4U -> 3R
+                Position {
+                    x: 51,
+                    y: 51 + (x - 1) % 50,
+                    orientation_idx: 0, //Right
+                }
+            } else {
+                assert_eq!(orientation, Orientation::Left);
+                if y <= 50 {
+                    // 1L -> 4R
+                    Position {
+                        x: 1,
+                        y: 150 - (y - 1) % 50,
+                        orientation_idx: 0, // Right
+                    }
+                } else {
+                    // 3L -> 4D
+                    assert!(y <= 100 && x <= 50);
+                    Position {
+                        x: 1 + (y - 1) % 50,
+                        y: 101,
+                        orientation_idx: 1, // Down
+                    }
+                }
+            }
+        };
+        if Orientation::Left == ORIENTATIONS[res.orientation_idx] {
+            assert_eq!(res.x % 50, 0);
+        }
+        if Orientation::Right == ORIENTATIONS[res.orientation_idx] {
+            assert_eq!(res.x % 50, 1);
+        }
+        if Orientation::Up == ORIENTATIONS[res.orientation_idx] {
+            assert_eq!(res.y % 50, 0);
+        }
+        if Orientation::Down == ORIENTATIONS[res.orientation_idx] {
+            assert_eq!(res.y % 50, 1);
+        }
+        res
     }
 
     pub fn clockwise(&self) -> Position {
@@ -95,7 +250,7 @@ fn parse_instructions(s: &str) -> Vec<Instruction> {
     res
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum MapLocation {
     Empty,
     Wall,
@@ -116,8 +271,6 @@ impl From<char> for MapLocation {
 fn get_dimensions(grid: &Vec<Vec<MapLocation>>) -> (usize, usize) {
     (grid[0].len(), grid.len())
 }
-
-// fn is_empty(map: &[String], p: &Position) -> bool {}
 
 pub fn run() {
     let lines = read_lines("in/day22.in").unwrap();
@@ -142,33 +295,75 @@ pub fn run() {
         .position(|x| x == &MapLocation::Empty)
         .unwrap();
 
+    {
+        let mut current_pos = Position {
+            x: start_x,
+            y: 0,
+            orientation_idx: 0,
+        };
+        for instruction in &instructions {
+            assert!(map[current_pos.y][current_pos.x] == MapLocation::Empty);
+            current_pos = match instruction {
+                Instruction::Clockwise => current_pos.clockwise(),
+                Instruction::Counterclockwise => current_pos.counter_clockwise(),
+                Instruction::Forward(n) => {
+                    for _ in 0..*n {
+                        let next_pos = current_pos.forward(&map);
+                        assert!(map[current_pos.y][current_pos.x] == MapLocation::Empty);
+                        if map[next_pos.y][next_pos.x] == MapLocation::Wall {
+                            break;
+                        }
+                        current_pos = next_pos;
+                    }
+                    assert!(map[current_pos.y][current_pos.x] == MapLocation::Empty);
+                    current_pos
+                }
+            }
+        }
+
+        println!(
+            "Day 22, part 1: {}",
+            1000 * (current_pos.y + 1) + 4 * (current_pos.x + 1) + current_pos.orientation_idx
+        );
+    }
+
+    // Part 2
+
+    // Pad everything
+    map.iter_mut().for_each(|r| {
+        r.insert(0, MapLocation::Padding);
+        r.push(MapLocation::Padding);
+    });
+    map.insert(0, [MapLocation::Padding; 3 * SIZE + 2].to_vec());
+    map.push([MapLocation::Padding; 3 * SIZE + 2].to_vec());
+
     let mut current_pos = Position {
-        x: start_x,
-        y: 0,
+        x: start_x + 1,
+        y: 1,
         orientation_idx: 0,
     };
     for instruction in &instructions {
-        assert!(map[current_pos.y][current_pos.x] == MapLocation::Empty);
+        assert_eq!(map[current_pos.y][current_pos.x], MapLocation::Empty);
         current_pos = match instruction {
             Instruction::Clockwise => current_pos.clockwise(),
             Instruction::Counterclockwise => current_pos.counter_clockwise(),
             Instruction::Forward(n) => {
                 for _ in 0..*n {
-                    let next_pos = current_pos.forward(&map);
-                    assert!(map[current_pos.y][current_pos.x] == MapLocation::Empty);
+                    let next_pos = current_pos.forward2(&map);
+                    assert_eq!(map[current_pos.y][current_pos.x], MapLocation::Empty);
                     if map[next_pos.y][next_pos.x] == MapLocation::Wall {
                         break;
                     }
                     current_pos = next_pos;
                 }
-                assert!(map[current_pos.y][current_pos.x] == MapLocation::Empty);
+                assert_eq!(map[current_pos.y][current_pos.x], MapLocation::Empty);
                 current_pos
             }
         }
     }
 
     println!(
-        "Day 22, part 1: {}",
-        1000 * (current_pos.y + 1) + 4 * (current_pos.x + 1) + current_pos.orientation_idx
+        "Day 22, part 2: {}",
+        1000 * current_pos.y + 4 * current_pos.x + current_pos.orientation_idx
     );
 }
